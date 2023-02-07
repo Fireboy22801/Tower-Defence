352578a0b7cc6b2372c2cf9405bc093b645ad887
Fireboy22801 <antoneskuoleksandr@gmail.com>
2023-02-07 10:42:34 +0000

    New Wave Spawner

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -1,145 +1,90 @@
-using UnityEngine;
 using System.Collections;
-using System.Linq;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
     public static int EnemiesAlive;
 
-    [Header("Main Settings")]
-    public List<Transform> enemiesPrefabs;
-    public List<float> spawnChances;
+    [SerializeField] private Waves[] _waves;
 
-    public float delay = 0.2f;
+    private int _currentEnemyIndex;
+    private int _currentWaveIndex;
+    private int _enemiesLeftToSpawn;
 
-    [Header("Q - the difference between chances (Geometric progression)")]
-    public int q = 3;
-
-    private int _waveIndex = 0;
-
-    private int qStart;
-    private bool isAutoStart = false;
+    private bool _isAutoStart = false;
 
     private void Start()
     {
-        qStart = q;
         EnemiesAlive = 0;
-        GetStartChances();
+        _enemiesLeftToSpawn = _waves[0].WaveSettings.Length;
     }
 
     private void Update()
     {
-        if (EnemiesAlive > 0)
+        if (_isAutoStart)
         {
-            return;
-        }
-
-        if (_waveIndex == 100)
-        {
-            Time.timeScale = 0;
-        }
-
-        if (isAutoStart)
-            StartCoroutine(SpawnWave());
-    }
-
-    IEnumerator SpawnWave()
-    {
-        PlayerStats.Rounds++;
-        _waveIndex++;
-
-        UpdateChances();
-
-        for (int i = 0; i < _waveIndex; i++)
-        {
-            SpawnEnemy();
-            yield return new WaitForSeconds(delay);
-        }
-    }
-    public void StartNextWave()
-    {
-        if (EnemiesAlive == 0)
-            StartCoroutine(SpawnWave());
-    }
-
-    public void AutoStart()
-    {
-        isAutoStart = !isAutoStart;
-    }
-
-    private void SpawnEnemy()
-    {
-        Instantiate(enemiesPrefabs[GetRandomEnemyIndex(spawnChances)], transform.position, Quaternion.identity);
-        EnemiesAlive++;
-    }
-
-    private int GetRandomEnemyIndex(List<float> chances)
-    {
-        float total = chances.Sum();
-        float rand = Random.Range(0f, total);
-        for (int i = 0; i < chances.Count; i++)
-        {
-            if (rand < chances[i])
-                return i;
-            total -= chances[i];
-            rand = Random.Range(0f, total);
-        }
-        return -1;
-    }
-
-    private void UpdateChances()
-    {
-        NormalizeChances();
-
-        for (int i = 0; i < spawnChances.Count; i++)
-        {
-            if (i >= spawnChances.Count / 2)
+            if (EnemiesAlive <= 0)
             {
-                if (_waveIndex >= 5)
-                    spawnChances[i] = (spawnChances[i] * 1.8f) + spawnChances[i - 1] / 100 * (spawnChances.Count - i);
-            }
-            else
-            {
-                if (spawnChances[i] <= 1)
-                {
-                    enemiesPrefabs.RemoveAt(i);
-                    spawnChances.RemoveAt(i);
-                }
+                LaunchWave();
+                EnemiesAlive = _enemiesLeftToSpawn;
             }
         }
 
-        NormalizeChances();
+
     }
 
-    private void GetStartChances()
+    private IEnumerator SpawnEnemyInWave()
     {
-        float sumOfGeometricProgr = (Mathf.Pow(q, spawnChances.Count) - 1) / (q - 1);
-
-        q = 1;
-
-        for (int i = spawnChances.Count - 1; i >= 0; i--)
+        if (_enemiesLeftToSpawn > 0)
         {
-            if (i >= spawnChances.Count / 2)
-                spawnChances[i] = 0;
-            else
+            yield return new WaitForSeconds(_waves[_currentWaveIndex]
+                .WaveSettings[_currentEnemyIndex]
+                .SpawnDelay);
+            Instantiate(_waves[_currentWaveIndex]
+                .WaveSettings[_currentEnemyIndex].Enemy,
+                transform.position, Quaternion.identity);
+            _enemiesLeftToSpawn--;
+            _currentEnemyIndex++;
+            StartCoroutine(SpawnEnemyInWave());
+        }
+        else
+        {
+
+            if (_currentWaveIndex < _waves.Length - 1)
             {
-                spawnChances[i] = 100f / sumOfGeometricProgr * q;
-                q *= qStart;
-                Mathf.Clamp(spawnChances[i], 0, Mathf.Infinity);
+                _currentWaveIndex++;
+                _enemiesLeftToSpawn = _waves[_currentWaveIndex].WaveSettings.Length;
+                _currentEnemyIndex = 0;
             }
         }
-
-        NormalizeChances();
     }
 
-    private void NormalizeChances()
+    public void LaunchWave()
     {
-        float sum = spawnChances.Sum();
-        float x = sum / 100;
-        for (int i = 0; i < spawnChances.Count; i++)
-        {
-            spawnChances[i] /= x;
-        }
+        EnemiesAlive = _waves[_currentWaveIndex].WaveSettings.Length;
+        StartCoroutine(SpawnEnemyInWave());
     }
+
+    public void AutoWaveSpawn()
+    {
+        _isAutoStart = !_isAutoStart;
+    }
+}
+
+[System.Serializable]
+public class Waves
+{
+    [SerializeField] private WaveSettings[] _waveSettings;
+    public WaveSettings[] WaveSettings { get => _waveSettings; }
+}
+
+[System.Serializable]
+public class WaveSettings
+{
+    [SerializeField] private GameObject _enemy;
+    public GameObject Enemy { get => _enemy; }
+    [SerializeField] private float _spawnDelay;
+    public float SpawnDelay { get => _spawnDelay; }
 }
